6369df511f0e2620cd3e8e6efe3dd8b43a6d2d43
jnike01 <bekennedy09@gmail.com>
2023-03-16 08:28:42 -0400

    fleshed out player house first floor a bit

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(TEST_TOWN_YOUR_HOUSE), MAP_NUM(TEST_TOWN_YOUR_HOUSE), WARP_ID_NONE, 5, 11);
+    SetWarpDestination(MAP_GROUP(TEST_TOWN_YOUR_HOUSE), MAP_NUM(TEST_TOWN_YOUR_HOUSE), WARP_ID_NONE, 8, 4);
     WarpIntoMap();
 }
 
